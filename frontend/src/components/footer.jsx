eed87871002ef478f8ebaf8c97a09e8828b80397
inigosto <demulcoder@gmail.com>
2024-01-05 11:35:27 +0300

    refactoring

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div>&#169; fbeatris</div>
+      <div>&#169; inigosto</div>
+      <br/>
+      <a href="https://github.com/WindDemulTeam/PeerStat">github</a>
+    </>
+  );
+};
+
+export default Footer;
